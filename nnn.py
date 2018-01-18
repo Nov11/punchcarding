4c77dc82ce8628e57340ec48f7569e8d04293a19
Nov11 <529079634@qq.com>
2018-01-19 02:05:54 +0800

    2018/01/19

---
+++ nnn.py
@@ -0,0 +1,60 @@
+class Solution:
+    def isMatch(self, s, p):
+        """
+        :type s: str
+        :type p: str
+        :rtype: bool
+        """
+        # print("s:{} p:{}\n".format(s,p))
+        if len(p) == 0:
+            return len(s) == 0
+
+        if len(p) == 1:
+            if len(s) != 1:
+                return False
+            return s[0] == p[0] or p[0] == '.'
+
+        if p[1] != '*':
+            if len(s) > 0 and (s[0] == p[0] or p[0] == '.'):
+                return self.isMatch(s[1:], p[1:])
+            else:
+                return False
+
+        # ---------------
+        # if self.isMatch(s, p[2:]):
+        #     return True
+        #
+        # for i in range(len(s)):
+        #     if s[i] == p[0] or p[0] == '.':
+        #         if self.isMatch(s[i + 1:], p[2:]):
+        #             return True
+        #     else:
+        #         break
+        #
+        # return False
+        # ------------------ these two are identical on probing sequence.
+        # ------------------ what makes the former one slower than the second one?
+        while len(s) > 0 and (s[0] == p[0] or p[0] == '.'):
+            if self.isMatch(s, p[2:]):
+                return True
+            s = s[1:]
+
+        return self.isMatch(s, p[2:])
+#         i guess string ops slows former procedure down
+
+
+if __name__ == '__main__':
+    s = Solution()
+
+    # assert (s.isMatch("aa", "a") == False);
+    # assert (s.isMatch("aa", "aa") == True);
+    # assert (s.isMatch("aa", ".*") == True);
+    # assert (s.isMatch("aa", "a*") == True)
+    # assert (s.isMatch("aaa", "aa") == False)
+    # assert (s.isMatch("ab", ".*") == True)
+    # assert (s.isMatch("aab", "c*a*b") == True)
+    # assert (s.isMatch("a", ".*..a*") == False)
+    # assert (s.isMatch("bbbba", ".*a*a") == True)
+
+# assert (s.isMatch("bbbacbcbcbbbbabbbab", "b*c*c*.*.*.*ab*c") == False)
+    print(s.isMatch("baccbbcbcacacbbc","c*.*b*c*ba*b*b*.a*"))
